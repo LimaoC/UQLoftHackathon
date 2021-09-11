aaecd25b63149875b681c4e85181cfcec0185cfd
Lewis Luck <lewisjluck@gmail.com>
2021-09-11 13:00:29 +1000

    Get papers test

--- app.py
+++ app.py
@@ -9,11 +9,20 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/get_courses", methods = ["GET", "POST"])
 def get_courses():
+    return "hello"
+
+@app.route("/is_course_valid", methods = ["GET", "POST"])
+def is_course_valid():
     code = request.json["courseCode"]
     return jsonify(is_valid_course(code))
 
 
-# @app.route("/get_papers", method = ["GET", "POST"])
-# def get_papers():
-#     if request.method == "POST":
-#         code = request.json["courseCode"]
+@app.route("/get_course_info", method = ["GET", "POST"])
+def get_papers():
+    if request.method == "POST":
+        data = {
+            "course_name": "Introduction to Computer Systems",
+            "course_description": "Richard Thomas is Cool" ,
+            "papers": [("2018", 2), ("2017", 1)]
+        }
+        return jsonify(data)
