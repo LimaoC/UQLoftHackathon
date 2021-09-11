46af7a213ea70e970d31a490d3aed3506f33fd51
Lewis Luck <lewisjluck@gmail.com>
2021-09-11 10:31:03 +1000

    CORS update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, jsonify
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/get_courses", methods = ["GET", "POST"])
 def get_courses():
