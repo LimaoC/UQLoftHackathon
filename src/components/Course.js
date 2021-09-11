f7171c8456e203b2957874818a7eddd0f7bb4c5d
LimaoC <80520563+LimaoC@users.noreply.github.com>
2021-09-11 12:53:29 +1000

    Courses

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Course({courseCode}) {
     return (
-        <div>{courseCode}</div>
+        <CourseStyled>
+            <h1>{courseCode.toUpperCase()}</h1>
+        </CourseStyled>
     );
 }
+
+
+const CourseStyled = styled.div`
+    h1 {
+        font-size: 3rem;
+        text-align: center;
+        margin-top: 3rem;
+    }
+`;
