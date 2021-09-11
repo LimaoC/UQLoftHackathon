34bc2fae654cc7cb24ba2c304f8f51fbc51bf9b0
LimaoC <80520563+LimaoC@users.noreply.github.com>
2021-09-11 17:17:37 +1000

    Update paper.js

--- src/components/Paper.js
+++ src/components/Paper.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { Document } from 'react-pdf';
 
-export default function Paper({ courseCode, paper}) {
+export default function Paper({courseCode, paper}) {
   const [pdfData, setPdfData] = useState("");
   useEffect(() => {
       const data = {"courseCode" : courseCode, "paper": paper};
@@ -22,7 +23,9 @@ export default function Paper({ courseCode, paper}) {
 
     return (
         <PaperStyled>
-            <h1> {pdfData}</h1>
+            {/* <h1>{pdfData}</h1> */}
+            <Document file="https://www.clickdimensions.com/links/TestPDFfile.pdf" />
+            hello
         </PaperStyled>
     )
 }
